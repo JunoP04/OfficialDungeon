e15ab44b7d57f98cd1e4966f0af1a99d65d6d47b
Will <wgrant14@hastings.edu>
2019-10-05 21:19:24 -0400

    Hover Color
    When u look at a cube, or mouse over it in arial view, it changes the color to purple while u hover.
    Note: you must switch views at least once for it to work... I don't know why.

---
+++ OfficialDungeonGame/Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    Ray ray;
+    RaycastHit hit;
+
+    //var rend = GetComponent.<Renderer>();
+
+    private void Start()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+    }
+
+
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit) && hit.collider.gameObject.tag == "Blue")
+        {
+            Renderer rend = GetComponent<Renderer>();
+
+            hit.collider.gameObject.GetComponent<Renderer>().material.shader = Shader.Find("RedST");
+            //Debug.Log(hit.collider.gameObject.name);
+        }
+    }
+}
