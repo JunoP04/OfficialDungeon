fd70ac4b2645a9d201b6a5b67923730831c7c778
Will <wgrant14@hastings.edu>
2019-10-08 13:31:14 -0400

    Spawn hearts
    Press H on any buildable space to turn it into a heart space

--- OfficialDungeonGame/Assets/Scripts/ColorChanger.cs
+++ OfficialDungeonGame/Assets/Scripts/ColorChanger.cs
@@ -7,13 +7,11 @@ public class ColorChanger : MonoBehaviour
     Ray ray;
     RaycastHit hit;
     public Material newMat;
-
-    //var rend = GetComponent.<Renderer>();
+    public GameObject heartPrefab;
 
     private void Start()
     {
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
     }
 
 
@@ -21,13 +19,39 @@ public class ColorChanger : MonoBehaviour
     {
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
+        //if a character looks at a buildable space, highlight it green
         if (Physics.Raycast(ray, out hit) && hit.collider.gameObject.tag == "Blue")
         {
-            //Renderer rend = GetComponent<Renderer>();
-            //Material newMat = new Material(source);
-            //newMat.color = Color.green;
+      
             hit.collider.gameObject.GetComponent<Renderer>().material = newMat;
-            //Debug.Log(hit.collider.gameObject.name);
         }
+
+
+        //if player presses spawn heart button AND is looking at a buildable cube.
+        if (Input.GetKeyDown(KeyCode.H) && Physics.Raycast(ray, out hit) && hit.collider.gameObject.tag == "Blue")
+        {
+            //get all the heart rooms and hearts already in the scene
+            //get the transform of whatever you are looking at
+            GameObject[] heartRooms = GameObject.FindGameObjectsWithTag("HeartRoom");
+            GameObject[] Hearts = GameObject.FindGameObjectsWithTag("Heart");
+            Transform target = hit.collider.gameObject.transform;
+            
+            //if there are any existing hearts/heart rooms, destroy them
+            if (heartRooms.Length > 0)
+            {
+                Destroy(Hearts[0]);
+                heartRooms[0].tag = "Blue";
+
+            }
+
+            //create an INSTANCE (this is important), of the heart
+            GameObject heartInstance = GameObject.Instantiate(heartPrefab, target) as GameObject;
+            //name it heart, because it kept adding "(copy)" to the end of the name, ie: Heart (copy), Heart (copy)(copy)... etc.
+            heartInstance.name = "Heart";
+            //retag the target room "heartRoom"
+            hit.collider.gameObject.tag = "HeartRoom";
+
+        }
+
     }
 }
