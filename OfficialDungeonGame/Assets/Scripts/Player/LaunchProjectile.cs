33d678bee9c4a31514d5d536e467f28040857b6d
JunoP04 <50187932+JunoP04@users.noreply.github.com>
2019-10-14 22:12:16 -0400

    Projectile scripts
    title

---
+++ OfficialDungeonGame/Assets/Scripts/Player/LaunchProjectile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchProjectile : MonoBehaviour
+{
+    public GameObject projectile;
+    public GameObject spawnPoint;
+    public float launchVelocity;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            ShootBall();
+        }
+    }
+
+    void ShootBall()
+    {
+        GameObject go = Instantiate(projectile, spawnPoint.transform.position, Quaternion.identity);
+        Rigidbody rb = go.GetComponentInChildren<Rigidbody>();
+        rb.AddForce(transform.forward * launchVelocity);
+        Destroy(go, 1.0f);
+    }
+}
