49001806fad11e1d5948f276f6e9749e103aefa0
Will <wgrant14@hastings.edu>
2019-11-21 19:33:01 -0500

    updated to master
    title

---
+++ OfficialDungeonGame/Assets/Scripts/Character/SpawnCorpse.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCorpse : MonoBehaviour
+{
+    public GameObject corpse;
+    public Vector3 deathPoint;
+
+    private void OnDestroy()
+    {
+        deathPoint = gameObject.GetComponent<Transform>().position;
+        Instantiate(corpse, deathPoint, Quaternion.identity);
+        Debug.Log("Corpse Spawned");
+    }
+}
